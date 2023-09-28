787bf0fa5489b808966f4b2b68986c9b312a7f56
Sarah Bernabe <76967872+sbernabe2@users.noreply.github.com>
2023-09-28 18:08:13 +0000

    Tentative solution problem 1

--- Scripts/problem1.sql
+++ Scripts/problem1.sql
@@ -1,3 +1,11 @@
 SELECT sql
 FROM sqlite_schema
 WHERE name = 'employee';
+
+SELECT employeeId, firstName, lastName, managerId
+FROM employee
+LIMIT 9;
+
+SELECT a.firstName || ' ' || a.lastName as employeeName, b.firstName || ' ' || b.lastName as managerName
+FROM employee a
+INNER JOIN employee b on b.managerId = a.managerId;
