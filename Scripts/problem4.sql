5eb35ed83334aeb0e07033476f3067a6a50d4c38
Sarah Bernabe <76967872+sbernabe2@users.noreply.github.com>
2023-09-29 13:14:13 +0000

    problem 4 solution again

--- Scripts/problem4.sql
+++ Scripts/problem4.sql
@@ -5,5 +5,16 @@ WHERE name = 'employee';
 SELECT e.employeeID, e.firstName, e.lastName, count(*) AS numberOfCarsSold
 FROM employee e 
 INNER JOIN sales s on e.employeeID = s.employeeID
-GROUP BY e.employeeId
+GROUP BY e.employeeId, e.firstName, e.lastName
+ORDER BY count(*) DESC, lastName ASC;
+
+SELECT e.employeeID, e.firstName, e.lastName
+FROM sales s
+INNER JOIN employee e 
+  on s.employeeID = e.employeeID
+
+SELECT e.employeeID, e.firstName, e.lastName, count(*) AS numberOfCarsSold
+FROM sales s
+INNER JOIN employee e on e.employeeID = s.employeeID
+GROUP BY e.employeeId, e.firstName, e.lastName
 ORDER BY count(*) DESC, lastName ASC;
